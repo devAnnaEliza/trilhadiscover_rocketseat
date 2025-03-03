2f2da92de841df07fbb0f0734a338051f7c9fc9a
Anna Eliza <annaeliza.als@gmail.com>
2025-03-03 18:45:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-
   html.classList.toggle('light')
+
+/* mudar a img de perfil na troca dark/light mode
+
+  const img = document.querySelector("#profile img")
+    if(html.classList.contains('light')) {
+
+    img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+
+      img.setAttribute("src", "./assets/avatar.png")
+    } 
+*/
 }
